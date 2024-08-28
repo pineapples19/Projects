b80ef88bb5a01725b05255ea235996afe0f7d688
Pineapples <128257851+pineapples19@users.noreply.github.com>
2024-08-28 21:56:40 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let slides = document.getElementsByClassName("carousel-item");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 4000); // Cambia la imagen cada 4 segundos
+}
